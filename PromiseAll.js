1399691c0f14866a4a82bd2e18ca455fc1c258a4
Alexandr Chernyakov <chernyakovit@gmail.com>
2023-09-22 00:56:07 +0300

    Create PromiseAll.js

---
+++ PromiseAll.js
@@ -0,0 +1,25 @@
+export default function promiseAll(iterable) {
+  return new Promise((resolve, reject) => {
+    const results = new Array(iterable.length);
+    let unresolved = iterable.length;
+
+    if (unresolved === 0) {
+      resolve(results);
+      return;
+    }
+
+    iterable.forEach(async (item, index) => {
+      try {
+        const value = await item;
+        results[index] = value;
+        unresolved -= 1;
+
+        if (unresolved === 0) {
+          resolve(results);
+        }
+      } catch (err) {
+        reject(err);
+      }
+    });
+  });
+}
